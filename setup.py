514fa73dc05228beefd5dc41dde7e2ab462a4fd4
hellogoose <60927126+hellogoose@users.noreply.github.com>
2020-04-26 14:41:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="dogebot",
       version="1.0",
-      py_modules=["doge"],
+      py_modules=["dogebot"],
       description="a chatbot based on the internet's beloved doge",
       author="hellogoose"
       )
